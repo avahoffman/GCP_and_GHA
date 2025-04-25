6e2fd4fef7f98131a0e5223d9b066f89be65d696
avahoffman <avamariehoffman@gmail.com>
2025-04-25 15:26:34 -0400

    Add in SA for gargle

--- R/render.R
+++ R/render.R
@@ -1,5 +1,14 @@
 
 install.packages("googlesheets4", repos = "https://cloud.r-project.org")
 library(googlesheets4)
+library(optparse)
+
+# Look for the sa argument
+option_list <- list(optparse::make_option(c("--sa"), type = "character", default = NULL))
+service_account <- parse_args(OptionParser(option_list = option_list))$sa
+
+# Authenticate
+options(gargle.gce.use_ip = TRUE)
+t <- gargle::credentials_gce(service_account)
 
 gs4_auth()
