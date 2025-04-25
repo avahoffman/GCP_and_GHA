1575736bff1499e2dc293f52ae446fcc297b1a83
avahoffman <avamariehoffman@gmail.com>
2025-04-25 17:51:47 -0400

    is it finding the creds?

--- R/render.R
+++ R/render.R
@@ -1,18 +1,22 @@
 
-install.packages(c("googlesheets4", "optparse"), repos = "https://cloud.r-project.org")
+install.packages("googlesheets4", repos = "https://cloud.r-project.org")
 library(googlesheets4)
-library(optparse)
+#library(optparse)
 
 # Look for the sa argument
-option_list <- list(optparse::make_option(c("--sa"), type = "character", default = NULL))
-service_account <- parse_args(OptionParser(option_list = option_list))$sa
+# option_list <- list(optparse::make_option(c("--sa"), type = "character", default = NULL))
+# service_account <- parse_args(OptionParser(option_list = option_list))$sa
 
 # Authenticate
 gs4_deauth()
-gs4_auth(
-  token =
-    gargle::credentials_service_account(
-      path = grep(".json$", list.files(), value = TRUE),
-      scopes = "https://www.googleapis.com/auth/spreadsheets"
-    )
-)
+t <- gargle::credentials_service_account(path = grep(".json$", list.files(), value = TRUE),
+                                         scopes = "https://www.googleapis.com/auth/spreadsheets")
+is.null(t)
+
+# gs4_auth(
+#   token =
+#     gargle::credentials_service_account(
+#       path = grep(".json$", list.files(), value = TRUE),
+#       scopes = "https://www.googleapis.com/auth/spreadsheets"
+#     )
+# )
